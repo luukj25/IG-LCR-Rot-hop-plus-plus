dac1c42ec5d34cba584b5495d302fdf675ea3c53
luukj25 <luuk.jille@gmail.com>
2026-06-19 22:35:17 +0200

    Add files via upload

---
+++ model/__init__.py
@@ -0,0 +1,3 @@
+# https://github.com/wesselvanree/LCR-Rot-hop-ont-plus-plus
+from .embeddings_layer import EmbeddingsLayer
+from .lcr_rot_hop_plus_plus import LCRRotHopPlusPlus
---
+++ model/bert_encoder/__init__.py
@@ -0,0 +1,3 @@
+# https://github.com/wesselvanree/LCR-Rot-hop-ont-plus-plus
+
+from .bert_encoder import BertEncoder, BertEncoderArgs
